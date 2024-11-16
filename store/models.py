1ad438402db6a996154778ae828011867a423253
EmreAnliacik <emreanliacik@hotmail.com>
2024-11-16 22:13:35 +0100

    1-Images linked for every product.2-Added property for images to avoid null image error.

--- store/models.py
+++ store/models.py
@@ -17,6 +17,14 @@ class Product(models.Model):
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
     def __str__(self):
         return self.name
 
