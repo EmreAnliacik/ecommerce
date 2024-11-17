c25e029f606bb7d8d81c5f79d4067767d6cdb991
acelya23 <acelya.caliskan2003@outlook.com>
2024-11-17 20:40:28 +0300

    processOrder

--- store/views.py
+++ store/views.py
@@ -114,4 +114,35 @@ def processOrder(request):
             )
     else:
         print('User is not logged in..')
+
+        print('COOKIES:' , request.COOKIES)
+        name=data['form']['name']
+        email=data['form']['email']
+
+        cookieData=cookieCart(request)
+        items=cookieData['items']
+
+        costumer, created = Costumer.objects.get_or_create(
+            email=email,
+        )
+       customer.name=name
+       customer.save()
+       order=Order.objects.create(
+           customer=customer,
+           complete=False
+       )
+     for item in items:
+         product=Product.objects.get(id=item['product']['id'])
+
+         orderItem=OrderItem.objects.create(
+             product=product,
+             order=order,
+             quantity=item['quantity']
+         )
+         total = float(data['form']['total'])
+         order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+        order.save()
     return JsonResponse('Payment complete!', safe=False)
