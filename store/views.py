250d8214421633009c770d8954fab5a219738996
acelya23 <acelya.caliskan2003@outlook.com>
2024-11-17 19:30:33 +0300

    cookieCart added

--- store/views.py
+++ store/views.py
@@ -3,6 +3,11 @@ from django.http import JsonResponse
 import json
 import datetime
 
+from .models import *
+from .utils import cookieCart
+
+#Create your views here.
+
 from django.shortcuts import render
 from .models import *
 
