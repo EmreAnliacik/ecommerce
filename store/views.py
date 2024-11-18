913a6cee25c2650ecf70577ee38d73216cbe2c87
EmreAnliacik <emreanliacik@hotmail.com>
2024-11-18 15:49:24 +0100

    Wiews.py fixed processOrder first part. -AHMET AYGULER

--- store/views.py
+++ store/views.py
@@ -1,15 +1,10 @@
-from itertools import product
 from django.http import JsonResponse
 import json
 import datetime
-
 from .models import *
-from .utils import cookieCart
-
-#Create your views here.
-
+from .utils import cookieCart, cartData, guestOrder
 from django.shortcuts import render
-from .models import *
+
 
 
 # Create your views here.
@@ -95,50 +90,32 @@ def processOrder(request):
 
     if request.user.is_authenticated:
         customer = request.user.customer
-    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    else:
+        customer, order = guestOrder(request, data)
+
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
 
     if total == order.get_cart_total:
         order.complete = True
-        order.save()
 
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                adress=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+        customer=customer,
+        order=order,
+        adress=data['shipping']['address'],
+        city=data['shipping']['city'],
+        state=data['shipping']['state'],
+        zipcode=data['shipping']['zipcode'],
             )
     else:
         print('User is not logged in..')
 
         print('COOKIES:' , request.COOKIES)
-        name=data['form']['name']
-        email=data['form']['email']
 
-        cookieData=cookieCart(request)
-        items=cookieData['items']
-
-        costumer, created = Costumer.objects.get_or_create(
-            email=email,
-        )
-       customer.name=name
-       customer.save()
-       order=Order.objects.create(
-           customer=customer,
-           complete=False
-       )
-     for item in items:
-         product=Product.objects.get(id=item['product']['id'])
-
-         orderItem=OrderItem.objects.create(
-             product=product,
-             order=order,
-             quantity=item['quantity']
-         )
          total = float(data['form']['total'])
          order.transaction_id = transaction_id
 
