58c7b76a7bff16ea0fba715a8a444835d66fcf42
AhmetAyguler <ahmetayguler@hotmail.com>
2024-11-21 15:06:12 +0100

    Word quessing game urls connected and added

--- store/urls.py
+++ store/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
     path('login/', views.login_view, name='login'),
+    path('word-guess/', views.word_guess_game, name='word_guess_game'),
 
 ]
